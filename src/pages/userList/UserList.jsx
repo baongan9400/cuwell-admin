be1dbb7bed562e95ceb32603c7157829d935dd40
leanhhuy103161 <leanhhuy103161@gmail.com>
2021-12-16 23:35:37 +0700

    feat: config api for get user list

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -3,11 +3,28 @@ import { DataGrid } from "@material-ui/data-grid";
 import { DeleteOutline } from "@material-ui/icons";
 import { userRows } from "../../dummyData";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MainLayout from "../../Layouts/MainLayout";
+import userApi from "../../api/user";
 
 export default function UserList() {
-  const [data, setData] = useState(userRows);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetchListUser();
+  }, []);
+
+  const fetchListUser = async () => {
+    try {
+      const response = await userApi.getAllUsers();
+      console.log(response);
+      if (response) setData(response);
+    } catch (error) {
+      console.log("failed to fetch list user: ", error);
+
+      // if failed to fetch, use dummy data
+      setData(userRows);
+    }
+  };
 
   const handleDelete = (id) => {
     setData(data.filter((item) => item.id !== id));
