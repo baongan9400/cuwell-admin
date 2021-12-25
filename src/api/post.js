08ed67083e8de0c985be1131d52bee5132fdb295
baongan9400 <dangbaongan9400@gmail.com>
2021-12-26 00:06:15 +0700

    block post, get post, user

--- src/api/post.js
+++ src/api/post.js
@@ -1,10 +1,26 @@
 import axiosManagement from "./axiosAuthenService";
 
 const postApi = {
-    getSearchPostData :(requestParams) => {
-        const url = `post-service/posts/`;
-        return axiosManagement.get(url, { params: requestParams });
-      }
+  getSearchPostData: (requestParams) => {
+    const url = `post-service/posts/`;
+    return axiosManagement.get(url, { params: requestParams });
+  },
+  blockPost: (pid) => {
+    let formData = new FormData();
+    formData.append("is_blocked", true);
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+    const url = `post-service/posts/${pid}/block/`;
+    return axiosManagement.put(url, formData, config);
+  },
+  getPostById: (pid) => {
+    console.log("call api");
+    const url = `post-service/posts/${pid}/`;
+    return axiosManagement.get(url);
+  },
 };
 
 export default postApi;
